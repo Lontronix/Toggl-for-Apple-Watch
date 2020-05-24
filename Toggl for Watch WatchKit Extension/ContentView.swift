89c0715de9ff77e28a44be0f603f30ddfc9dc67a
Lonnie Gerol <lonniegerol@gmail.com>
2020-05-23 22:00:47 -0500

    Initial Commit

---
+++ Toggl for Watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Toggl for Watch WatchKit Extension
+//
+//  Created by Lonnie Gerol on 5/23/20.
+//  Copyright © 2020 Lontronix. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Toggl for Watch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Toggl for Watch WatchKit Extension
+//
+//  Created by Lonnie Gerol on 5/23/20.
+//  Copyright © 2020 Lontronix. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
