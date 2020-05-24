2a916ac168ce67a3584fa3efb2f4a20c5e73ae0a
Lonnie Gerol <lonniegerol@gmail.com>
2020-05-23 23:17:00 -0500

    created a temporary layout for sign in screen

--- Toggl for Watch/ContentView.swift
+++ Toggl for Watch/ContentView.swift
@@ -9,13 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var emailAddress: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("Welcome")
+                .font(.title)
+            Text("Please Log in with your Toggl account")
+                .font(.body)
+
+            Spacer()
+
+                VStack(alignment: .leading){
+                        Text("Email Address")
+                        TextField("email address", text: self.$emailAddress)
+                            .frame(width: nil, height: 44, alignment: .center)
+                        Text("Password")
+                        TextField("password", text: self.$password)
+                            .frame(width: nil, height: 44, alignment: .center)
+                }
+                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+            Spacer()
+            VStack{
+                Button(action: {
+                    print("ligma")
+                }) {
+                    Text("Sign in")
+                        .foregroundColor(.white)
+                        .frame(width: nil, height: 44, alignment: .center)
+                        .background(Color.red)
+                        .offset(x: 0, y: -10)
+
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group{
+            ContentView()
+                .previewDevice(PreviewDevice(stringLiteral: "iPhone SE"))
+                .previewDisplayName("iPhone SE")
+            ContentView()
+                .previewDevice(PreviewDevice(stringLiteral: "iPhone 11"))
+                .previewDisplayName("iPhone 11")
+            ContentView()
+                .previewDevice(PreviewDevice(stringLiteral: "iPhone 11 Pro Max"))
+                .previewDisplayName("iPhone 11 Pro Max")
+        }
     }
 }
